216b74a452aa9d7754427c8d396bf4883046b4f6
PoseyXyz <adeposi60@gmail.com>
2023-07-28 17:06:32 +0200

    Added gpt tester file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
             <div className='flex max-w-6xl container flex-col gap-4'>
                 <h6 className='font-semibold'>Hi, My name is</h6>
                 <h2 className='text-black font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Sydney Idundun.</h2>
-                <h2 className='text-[#111111] text-4xl sm:text-5xl font-bold md:text-6xl lg:text-7xl'>Your next <span className='underline animate-pulse'>creative</span> developer</h2>
+                <h2 className='text-[#111111] text-4xl sm:text-5xl font-bold md:text-6xl lg:text-7xl'>Your next <span className='underline animate-pulse'>pragmatic</span> developer</h2>
                 <p className='max-w-3xl pt-8 pb-9 leading-6 md:text-lg font-normal dark:text-brand-grey-alt'>{`Hi, my name's Sydney, a pragmatic`} <strong className='animate-pulse'>fullstack software developer</strong> {`with experience creating REST API's using django and drf to create`}<strong className='animate-pulse'>interactive</strong> and <strong className='animate-pulse'>scalable</strong> web solutions and experiences that <strong className='animate-pulse'>solve problems</strong> and <strong className='animate-pulse'>drive innovation</strong>.</p>
                 {/* <div className='flex mt-2 gap-4'><Link href="/#projects"><a className="hero-link">View My Projects</a></Link><span>or</span><Link href="/#about"><a className="hero-link before:right-0">Read About Me</a></Link></div> */}
                 {/* <a target="_blank" rel="noopener noreferrer" href='mailto:adeposi60@gmail.com' className="bg-black self-start text-white rounded-md py-3 px-10 text-xl hover:bg-white hover:text-black border hover:animate-pulse hover:border-black border-black duration-200">Reach Out</a> */}
---
+++ pages/gpt.tsx
@@ -0,0 +1,62 @@
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+
+
+export default function Home() {
+    const [loading, setLoading] = useState(true)
+    const [aiTextResponse, setAiTextResponse] = useState<{ [fieldName: string]: string }>({
+        name: '',
+        description: '',
+        languages_used: '',
+    })
+
+    const [ai_queries, setAiQueries] = useState<{ [fieldName: string]: string }>({
+        name: 'Generate a name for a software development project which would be suitable for the online portfolio of a web software developer',
+        languages_used: 'Generate a list of programming languages which can be used for a software developers project'
+    })
+
+
+    function airequestsender() {
+
+        const configuration = new Configuration({
+            
+            apiKey: `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`,
+        });
+        const openai = new OpenAIApi(configuration);
+
+        async function runCompletion() {
+            setLoading(true)
+            let tempObject: { [fieldName: string]: string } = {}
+            
+                try {
+                    
+                    const completion = await openai.createChatCompletion({
+                        model: "gpt-3.5-turbo",
+                        messages: [
+                         {role:"system", content:"You are a helpful artificial intelligence assistant that is meant for the purpose of generating project ideas and descriptions for the portfolio websites of software developer"},
+                         {role: "user", content:'Generate a list of programming languages which can be used for a software developers project' },
+                        ],
+                        max_tokens: 3000,
+                        temperature:1.1
+                    });
+                    console.log(completion.data.choices[0].message.content!);
+                   
+                    
+
+                } catch (error) {
+                    console.log(error);
+                }
+            
+            setAiTextResponse(tempObject)
+            setLoading(false)
+        }
+        runCompletion()
+        console.log(aiTextResponse);
+        
+    }
+  return (
+    <div>
+        <button onClick={airequestsender} className="bg-black text-white p-4 text-xl">Test API response</button>
+    </div>
+  )
+}
